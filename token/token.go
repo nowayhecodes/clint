bfe21eec0f66a01ff820be1332b9a22a08640a28
Gustavo Cavalcante <nowayhecodes@gmail.com>
2021-01-17 03:49:01 -0300

    Add tokenizer

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// TokenType ...
+type TokenType string
+
+// Token ...
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	IDENT   = "IDENT"
+	INT     = "INT"
+	FLOAT   = "FLOAT"
+	STR     = "STRING"
+	ASSIGN  = "="
+	PLUS    = "+"
+	MINUS   = "-"
+	MULT    = "*"
+	DIV     = "/"
+	POW     = "^"
+	COMMA   = ","
+	COLON   = ":"
+	SEMI    = ";"
+	LPAREN  = "("
+	RPAREN  = ")"
+	LBRACE  = "{"
+	RBRACE  = "}"
+	MODULE  = "MODULE"
+	CLASS   = "CLASS"
+	FUN     = "FUNCTION"
+	LET     = "LET"
+)
